85bc587835324f02d8cfb0efe218fe43862f0033
ishanwaghela2007 <160811456+ishanwaghela2007@users.noreply.github.com>
2024-08-17 10:35:47 +0530

    updated code

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   const [user,setuser]=useState(120)
   const [client,setclient]=useState(100)
+  const [trade,settrade]=useState(98)
   return (
     <div className="min-h-screen bg-gray-100">
       {/* Navbar */}
@@ -45,22 +46,27 @@ const Home = () => {
         className="container mx-auto px-6 py-12 bg-cover bg-center h-screen bg-transparent"
         style={{
           backgroundImage:
-            "url(https://th.bing.com/th/id/OIG3.EzZ68hwdcGqErO3SwkJT?pid=ImgGn)",
+            "url(https://th.bing.com/th/id/OIG1.AVkPD3lkOyD_rE7EWjd0?pid=ImgGn)",
 
         }}
       >
         <div className="px-6 py-6 mt-6 text-center">
-          <h1 className="text-3xl font-bold text-black">
-            Welcome to
+          <h1 className="text-3xl font-bold text-white">
+            Cultivate Success with 
             <span className="flex justify-center items-center"></span>
             Fitlife
           </h1>
+          <div className="text-center text-white">
+          <p>Join our ever growing platform to become fit</p>
+          <p>It is open source</p>
+          </div>
+
         </div>
       </div>
       </>
       <>
       <div className="w-80 h-auto bg-white text-black p-6 border border-transparent rounded-xl shadow-xl mx-auto transform hover:scale-105 transition-transform duration-300">
-  <img className="h-24 w-24 mx-auto mb-4 rounded-full border-4 border-white shadow-lg" src="https://static.vecteezy.com/system/resources/previews/007/296/447/original/user-icon-in-flat-style-person-icon-client-symbol-vector.jpg" alt="user icon"/>
+  <img className="h-24 w-24 mx-auto mb-4 rounded-full border-4 border-white shadow-lg" src="https://static.vecteezy.com/system/resources/thumbnails/005/545/335/small/user-sign-icon-person-symbol-human-avatar-isolated-on-white-backogrund-vector.jpg" alt="user icon"/>
   
   <p className="text-center text-2xl font-semibold mb-2">{user}k</p>
   <h2 className="text-center text-lg font-medium">Active Users within 30 days</h2>
@@ -75,6 +81,14 @@ const Home = () => {
   <h2 className="text-center text-lg font-medium">Client</h2>
 </div>
 
+      </>
+      <>
+      <div className="w-80 h-auto bg-white text-black p-6 border border-transparent rounded-xl shadow-xl mx-auto transform hover:scale-105 transition-transform duration-300">
+  <img className="h-24 w-24 mx-auto mb-4 rounded-full border-4 border-white shadow-lg" src="https://static.vecteezy.com/system/resources/previews/007/296/447/original/user-icon-in-flat-style-person-icon-client-symbol-vector.jpg" alt="client icon"/>
+  
+  <p className="text-center text-2xl font-semibold mb-2">{trade}k</p>
+  <h2 className="text-center text-lg font-medium">Trade accros world</h2>
+</div>
       </>
       <>
       {/* <Footer/> */}
